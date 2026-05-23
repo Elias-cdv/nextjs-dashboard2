e607e195e459b7ae690a63064e8836bfb456b23c
Elías Cabeza de Vaca <elways38@gmail.com>
2026-05-23 17:23:57 -0500

    feat(metadata): add metadata definition for invoices and layout pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
+import { Metadata } from "next"; // 1. Importa esto
+
+// 2. Define el objeto metadata
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Acme Dashboard", // El %s se sustituye por el título de la página
+    default: "Acme Dashboard", // Si una página no tiene título, usa este
+  },
+  description: "The official Next.js Course Dashboard, built with App Router.",
+  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
 
 export default function RootLayout({
   children,

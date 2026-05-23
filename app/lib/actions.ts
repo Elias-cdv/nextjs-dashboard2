c81826e387ecee1fc00857c4b9a01d06b7dc418c
Elías Cabeza de Vaca <elways38@gmail.com>
2026-05-23 16:55:48 -0500

    feat(auth): implement authentication with NextAuth and credentials provider
    - Added `auth.config.ts` to configure NextAuth with custom pages and callbacks.
    - Created `auth.ts` to handle user authentication using credentials and bcrypt for password verification.
    - Introduced middleware in `middleware.ts` to protect routes and manage authentication state.
    - Updated login form to handle authentication errors and redirect users appropriately.
    - Enhanced side navigation to include sign-out functionality.
    - Updated dependencies in `package.json` and `pnpm-lock.yaml` for compatibility with new features.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -98,3 +98,22 @@ export type State = {
   };
   message?: string | null;
 };
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
+}

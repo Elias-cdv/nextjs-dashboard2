61a0733cca53058ed33a3a8d94e16dc0f97ecea8
Elías Cabeza de Vaca <elways38@gmail.com>
2026-05-23 17:57:00 -0500

    feat(auth): update auth configuration to include secure cookies setting

--- auth.config.ts
+++ auth.config.ts
@@ -1,21 +1,24 @@
+// auth.config.ts
 import type { NextAuthConfig } from "next-auth";
 
 export const authConfig = {
   pages: {
     signIn: "/login",
   },
+  // ESTO OBLIGA A RECONOCER LAS COOKIES DE TU PANTALLA:
+  useSecureCookies: false,
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
       if (isOnDashboard) {
         if (isLoggedIn) return true;
-        return false; // Redirige a los no logueados al login
+        return false;
       } else if (isLoggedIn) {
         return Response.redirect(new URL("/dashboard", nextUrl));
       }
       return true;
     },
   },
-  providers: [], // Se llena en auth.ts
+  providers: [],
 } satisfies NextAuthConfig;

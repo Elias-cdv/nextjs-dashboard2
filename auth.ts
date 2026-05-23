8886267c02a0590af117f1d15cb0e64b5ca5139f
Elías Cabeza de Vaca <elways38@gmail.com>
2026-05-23 18:15:50 -0500

    fix(auth): update user retrieval logic and improve error handling feat(dependencies): upgrade next-auth and postgres packages

--- auth.ts
+++ auth.ts
@@ -4,12 +4,14 @@ import { authConfig } from "./auth.config";
 import { z } from "zod";
 import type { User } from "@/app/lib/definitions";
 import bcrypt from "bcrypt";
-import { sql } from "@vercel/postgres";
+import postgres from "postgres";
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 async function getUser(email: string): Promise<User | undefined> {
   try {
     const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
-    return data.user[0];
+    return user[0];
   } catch (error) {
     console.error("Failed to fetch user:", error);
     throw new Error("Failed to fetch user.");

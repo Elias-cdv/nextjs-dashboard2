4a2b3d34680d418f3fe5790d60d009a831f8dbab
Elías Cabeza de Vaca <elways38@gmail.com>
2026-05-23 17:59:56 -0500

    call

--- auth.config.ts
+++ auth.config.ts
@@ -1,22 +1,12 @@
-// auth.config.ts
 import type { NextAuthConfig } from "next-auth";
 
 export const authConfig = {
   pages: {
     signIn: "/login",
   },
-  // ESTO OBLIGA A RECONOCER LAS COOKIES DE TU PANTALLA:
-  useSecureCookies: false,
   callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false;
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL("/dashboard", nextUrl));
-      }
+    authorized() {
+      // ESTO DEJA PASAR A CUALQUIERA SIN PEDIR CONTRASEÑA
       return true;
     },
   },

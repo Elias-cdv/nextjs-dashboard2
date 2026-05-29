71d2fec61efdbce5e9af87f78552c285b506f1e3
Elías Cabeza de Vaca <elways38@gmail.com>
2026-05-28 21:40:23 -0500

    modify middelware

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,21 @@
+// middleware.ts (en la raíz)
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 
-export default NextAuth(authConfig).auth;
+// Usamos ./auth si exportas desde auth.ts, o authConfig directamente
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+  const isLoggedIn = !!req.auth;
+  const isOnDashboard = req.nextUrl.pathname.startsWith("/dashboard");
+
+  if (isOnDashboard) {
+    if (isLoggedIn) return true;
+    return Response.redirect(new URL("/login", req.nextUrl));
+  }
+  return true;
+});
 
 export const config = {
-  // https://nextjs.org/docs/app/api-reference/file-conventions/proxy#matcher
   matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 };
